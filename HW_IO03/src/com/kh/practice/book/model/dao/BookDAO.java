f060ef600140ee7b3571da5e4a3da3e3066919d2
user1 <user1@DESKTOP-KJ5DDVU>
2026-01-09 17:50:21 +0900

    책?

--- HW_IO03/src/com/kh/practice/book/model/dao/BookDAO.java
+++ HW_IO03/src/com/kh/practice/book/model/dao/BookDAO.java
@@ -1,5 +1,45 @@
 package com.kh.practice.book.model.dao;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
+import com.kh.practice.book.model.vo.Book;
+
 public class BookDAO {
 
+	private Book[] bArr = new Book[10];
+	
+
+	public void fileSave(Book[] bArr) {
+		
+		try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("book.txt")) ){
+			
+			
+			
+			
+			
+			
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public Book[] fileRead() {
+		
+		
+		Book[] b = new Book[10];
+		
+		
+		return b;
+	}
+	
 }
